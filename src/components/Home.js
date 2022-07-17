da70587cd42db42e1512759d0c3599d786defc9c
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-07-17 18:14:44 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import React from "react";
 function Home() {
   return (
     <div id="home">
-      <h1>
+      <h1 style={{color:"firebrick"}}>
         {username} is a Web Developer from {city}
       </h1>
     </div>
